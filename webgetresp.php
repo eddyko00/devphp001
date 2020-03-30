5e8c1d486e83e5558390bfeab95b7a0aa9236161
koed <koed@L097105.corp.ads>
2020-03-30 17:27:22 -0400

    graph

--- webgetresp.php
+++ webgetresp.php
@@ -19,12 +19,15 @@ if(!empty($_GET['cmd'])) {
 //$password = "admin";
 //$DBName = "sampledb";
 
-$host = "remotemysql.com";
-$username = "sETFHeGQOd";
-$password = "n2wkEU8OXY";
-$DBName = "sETFHeGQOd";
+//$host = "remotemysql.com";
+//$username = "sETFHeGQOd";
+//$password = "n2wkEU8OXY";
+//$DBName = "sETFHeGQOd";
 
-
+$host = "www.db4free.net";
+$username = "eddysa";
+$password = "eddyadmin";
+$DBName = "eddydb_sample";
 
 $conn = mysqli_connect($host,$username,$password) or die(mysql_error());
 $db = mysqli_select_db($conn,$DBName) or die(mysql_error());
